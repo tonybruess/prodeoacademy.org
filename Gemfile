69bd2361f09361d906b9dcce2aa10f54aeca4059
mrapple <tony@oc.tc>
2012-11-01 18:04:18 -0500

    Use compass and sass for CSS

--- Gemfile
+++ Gemfile
@@ -3,3 +3,6 @@ source 'http://rubygems.org'
 gem 'staticmatic2'
 gem 'haml'
 gem 'capistrano'
+gem 'compass'
+gem 'compass_twitter_bootstrap'
+
--- config/site.rb
+++ config/site.rb
@@ -1,3 +1,6 @@
+require 'compass'
+require 'compass_twitter_bootstrap'
+
 configuration.preview_server_port = 4000
 configuration.default_template_engine = :haml
 configuration.preview_server_host = "localhost"
