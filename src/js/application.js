1f20e4fda9ba02ee4d9a4d10452162fcc4afd7c2
mrapple <tony@oc.tc>
2012-11-09 16:51:06 -0600

    Finish up home page

--- src/js/application.js
+++ src/js/application.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
     $("[rel=tooltip]").tooltip({placement: 'right'});
     $('#carousel').carousel()
+
 });
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=185949604491";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
