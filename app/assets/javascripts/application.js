e1ff8ff843cf8f6f8a1549416fc4103fb3dcb46f
mrapple <tony@oc.tc>
2014-02-08 22:02:19 -0600

    Add carousel images, fix tenses, rearrange top

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,11 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require twitter/bootstrap/transition
 //= require_tree .
 
 $(document).ready(function() {
     $("[rel=tooltip]").tooltip({placement: 'right'});
-    $('#carousel').carousel()
+    $('#carousel').carousel();
 
 });
 
